51d45ab31fd82ebaf04e17f37ed64474b792cb4f
Hfdbkm <hfdbkmIfrbhpzyjd@gmail.com>
2021-09-15 20:25:04 +0300

    test badges

--- src/Differ.php
+++ src/Differ.php
@@ -8,20 +8,15 @@ function fileDiff(string $filepath1, string $filepath2, string $format = 'stylis
 {
     $file1 = parse($filepath1);
     $file2 = parse($filepath2);
-    $keys1 = array_keys($file1);
-    $keys2 = array_keys($file2);
-    $keys = array_merge($keys1, $keys2);
-    $keys = array_unique($keys);
+    $keys = array_unique(array_merge(array_keys($file1), array_keys($file2)));
     sort($keys);
     $res = [];
     foreach ($keys as $key) {
-        if (array_key_exists($key, $file1) && array_key_exists($key, $file2)) {
-            if ($file1[$key] === $file2[$key]) {
-                $res[] = "  {$key}: $file1[$key]";
-            } else {
+        if ($file1[$key] === $file2[$key]) {
+            $res[] = "  {$key}: $file1[$key]";
+        } elseif (array_key_exists($key, $file1) && array_key_exists($key, $file2)) {
                 $res[] = "- {$key}: $file1[$key]";
                 $res[] = "+ {$key}: $file2[$key]";
-            }
         } elseif (!array_key_exists($key, $file1)) {
             $res[] = "+ {$key}: $file2[$key]";
         } else {
